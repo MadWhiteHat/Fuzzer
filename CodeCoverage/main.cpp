5a4a212e47042d7b41bbdc6e0da49669a7d81ddd
MadWhiteHat <kozelkovmike@gmail.com>
2024-03-16 17:32:41 +0300

    Done: trace function dll

--- CodeCoverage/main.cpp
+++ CodeCoverage/main.cpp
@@ -1,71 +1,163 @@
-#include <iostream>
-#include <fstream>
+/*
+ * Copyright (C) 2004-2021 Intel Corporation.
+ * SPDX-License-Identifier: MIT
+ */
 
+ //
+ // This tool counts the number of times a routine is executed and
+ // the number of instructions executed in a routine
+ //
+
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cstring>
+#include <unordered_set>
 #include "pin.H"
 
+// Holds instruction count for a single procedure
+typedef struct RtnCount
+{
+  std::string _name;
+  ADDRINT _address;
+} RTN_COUNT;
 
-PIN_MUTEX mutex;
-std::ofstream out;
-static INT32 instCount = 0;
+// Linked list of instruction counts for each routine
+static std::vector<RTN_COUNT*> trace;
+bool enable_trace = false;
+static const std::unordered_set <std::string> func_to_track = {
+  "puts",
+  "printf",
+  "memset",
+  "memcpy",
+  "sprintf",
+  "exit",
+  "fopen",
+  "fseek",
+  "fread",
+  "fflush",
+  "fclose",
+  "strstr",
+  "strlen"
+};
 
-// This function is called before every instruction is executed
-static VOID AddInst() {
+void log_result(INT32 exit_code) {
+  std::ofstream out("trace.out");
+  if (out.is_open()) {
+    for (const auto& rc : trace) { out << rc->_address << '\n'; }
+    out << exit_code;
 
-  PIN_MutexLock(&mutex);
-  
-  ++instCount;
-  out << instCount << '\n';
-
-  PIN_MutexUnlock(&mutex);
+    out.close();
+  }
 }
 
-// Pin calls this function every time a new instruction is encountered
-static VOID Instruction(INS ins, VOID* v)
-{
-  // Insert a call to docount before every instruction, no arguments are passed
-  INS_InsertCall(ins, IPOINT_BEFORE, (AFUNPTR)AddInst, IARG_END);
+// This function is called before every instruction is executed
+static VOID add_rtnc(RTN_COUNT* rtnc) {
+  if (rtnc->_name == "main") { enable_trace = true; }
+  if (enable_trace) { trace.push_back(rtnc); }
+}
+
+static std::string get_module_name(RTN* rtn) {
+  std::string module_name = IMG_Name(SEC_Img(RTN_Sec(*rtn)));
+
+  auto pos = module_name.rfind('\\');
+  if (pos != std::string::npos && ++pos < module_name.length()) {
+    module_name = module_name.substr(pos);
+  }
+
+  return module_name;
+}
+
+static bool is_track_func(
+  const std::string& module_name, const std::string& func_name
+) {
+  if (module_name.ends_with(".exe")
+    || module_name == "func.dll"
+    || func_to_track.contains(func_name)
+  ) {
+    return true;
+  } else { return false; }
+}
+
+
+// Pin calls this function every time a new rtn is executed
+static VOID Routine(RTN rtn, VOID* v) {
+  // Allocate a counter for this routine
+
+  std::string module_name = get_module_name(&rtn);
+  std::string func_name = RTN_Name(rtn);
+
+  if (!is_track_func(module_name, func_name)) { return; }
+
+  // The RTN goes away when the image is unloaded, so save it now
+  // because we need it in the fini
+  RTN_COUNT* rc = new RTN_COUNT;
+
+  rc->_name = func_name;
+  rc->_address = RTN_Address(rtn);
+
+  RTN_Open(rtn);
+
+  // Insert a call at the entry point of a routine to increment the call count
+  RTN_InsertCall(rtn, IPOINT_BEFORE, (AFUNPTR)add_rtnc, IARG_PTR, rc, IARG_END);
+
+  RTN_Close(rtn);
+}
+
+static EXCEPT_HANDLING_RESULT ExceptionHandler(
+  THREADID tid,
+  EXCEPTION_INFO* pExcptionInfo,
+  PHYSICAL_CONTEXT* pPhysCont,
+  VOID* v
+) {
+
+  std::cout << "Caught" << std::endl;
+  PIN_ExitApplication(EXIT_SUCCESS);
 }
 
 // This function is called when the application exits
 static VOID Fini(INT32 code, VOID* v) {
-  PIN_MutexLock(&mutex);
-  out.close();
-  PIN_MutexUnlock(&mutex);
+  log_result(code);
 
-  PIN_MutexFini(&mutex);
+  for (auto& rc : trace) { delete(rc); }
 }
 
 /* ===================================================================== */
 /* Print Help Message                                                    */
 /* ===================================================================== */
 
-static INT32 Usage() {
-  std::cerr << "This tool counts the number of dynamic instructions executed" << std::endl;
-  return EXIT_FAILURE;
+static INT32 Usage()
+{
+  std::cerr << "This Pintool tracks subroutine calls after main" << std::endl;
+  std::cerr << std::endl << KNOB_BASE::StringKnobSummary() << std::endl;
+  return -1;
 }
 
 /* ===================================================================== */
 /* Main                                                                  */
 /* ===================================================================== */
-/*   argc, argv are the entire command line: pin -t <toolname> -- ...    */
-/* ===================================================================== */
 
-
-INT32 main(int argc, char** argv)
+int main(int argc, char* argv[])
 {
-  std::cout << "[CodeCoverage] Start..." << std::endl;
-  if (PIN_Init(argc, argv)) { return Usage(); }
+  // Initialize symbol table code, needed for rtn instrumentation
+  PIN_InitSymbols();
 
-  out.open("out.txt");
-  if (!out.is_open()) { return EXIT_FAILURE; }
+  // Initialize pin
+  if (PIN_Init(argc, argv)) return Usage();
 
-  if (!PIN_MutexInit(&mutex)) { return EXIT_FAILURE; }
+  trace.reserve(10000);
+  // Register Routine to be called to instrument rtn
+  RTN_AddInstrumentFunction(Routine, 0);
 
-  INS_AddInstrumentFunction(Instruction, 0);
+  // Register exception handler
+  PIN_AddInternalExceptionHandler(ExceptionHandler, 0);
+
+  // Register Fini to be called when the application exits
   PIN_AddFiniFunction(Fini, 0);
 
-  std::cout << "[CodeCoverage] Program trace Start" << std::endl;
-
+  // Start the program, never returns
   PIN_StartProgram();
-  std::exit(instCount);
+
+  return 0;
 }
